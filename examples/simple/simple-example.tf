624280d4cfdc1c69c9e2f486b664468614760837
yoshutch <yoshutch@gmail.com>
2021-05-17 15:43:25 -0600

    initial setup

--- examples/simple/simple-example.tf
+++ examples/simple/simple-example.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-module "<module_name>" {
-  source = "github.com/byu-oit/terraform-aws-<module_name>?ref=v1.0.0"
+module "domain-redirect" {
+  source = "github.com/byu-oit/terraform-aws-domain-redirect?ref=v1.0.0"
   #source = "../" # for local testing during module development
 }
