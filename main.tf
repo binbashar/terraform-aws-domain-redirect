b656f388e096fe3bccea8880a5b9bf8f2a3bceaa
Josh Gubler <josh.gubler@gmail.com>
2023-10-04 18:31:42 -0600

    don't create cloudfront until cert is validated

--- main.tf
+++ main.tf
@@ -132,7 +132,7 @@ resource "aws_cloudfront_distribution" "redirect" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.cert_us_east_1.arn
+    acm_certificate_arn      = aws_acm_certificate_validation.validation_us_east_1.certificate_arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.1_2016"
   }
